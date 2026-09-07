c918221f1e4b17fbbd8a0cd149f61cedce711256
SportAPI Developer <developer@sportapi.local>
2026-09-07 11:44:00 +0530

    .,.

--- AuthenticationExtensions.cs
+++ AuthenticationExtensions.cs
@@ -26,8 +26,7 @@ public static class AuthenticationExtensions
                     "https://login.microsoftonline.com/66c5be6d-b895-4170-9fe4-1d29253008f0/v2.0",
                     "https://sts.windows.net/66c5be6d-b895-4170-9fe4-1d29253008f0/"
                 },
-                ValidateAudience = true,
-                ValidAudience = backendClientId,
+                ValidateAudience = false,
                 ValidateLifetime = true,
                 ValidateIssuerSigningKey = true
             };
